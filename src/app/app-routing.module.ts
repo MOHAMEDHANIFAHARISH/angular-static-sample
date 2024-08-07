c40e4ce1fe072f60dce645c7c1dc2e5731a47c18
MOHAMED HANIFA HARISH <mohamedhanifaharish1@gmail.com>
2024-08-07 10:58:29 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
 import { ProductOverviewComponent } from './product-overview/product-overview.component';
 import { NavComponent } from './nav/nav.component';
+import { ProductComponent } from './product/product.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  {
-    path:'users', component:UsersComponent
-  },
-  { path: 'todos', loadChildren: () => import('./todos/todos.module').then(m => m.TodosModule) },
   {path:'',component:HomeComponent},
+  {path:'',component:NavComponent},
+  {path:'product',component:ProductComponent},
+  {path:'about',component:AboutComponent},
   {path:'home',component:HomeComponent},
-  {path:'product-overview',
-  component:ProductOverviewComponent},
-  {path:'',
-  component:NavComponent}
+  {path:'product-overview',component:ProductOverviewComponent},
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
 import { ProductOverviewComponent } from './product-overview/product-overview.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
     HomeComponent,
     ProductOverviewComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
