a9aef8bb3cb697a61361653e3c08df4b42e60510
MOHAMED HANIFA HARISH <mohamedhanifaharish1@gmail.com>
2024-08-08 16:32:07 +0530

    third commit

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+menuVariable:boolean = false;
+menu_icon_variable:boolean=false;
+menu_icon_variable1:boolean=true;
 
+
+openMenu(){
+  this.menuVariable =! this.menuVariable;
+  this.menu_icon_variable=! this.menu_icon_variable;
+  this.menu_icon_variable1=!this.menu_icon_variable1
 }
+
+closeNav() {
+  this.menuVariable = false;
+  this.menu_icon_variable1=true;
+  this.menu_icon_variable = false;
+}
+}
+
+// isNavOpen = false;
+
+// toggleNav() {
+//   this.isNavOpen = !this.isNavOpen;
+// }
+
+// closeNav() {
+//   this.isNavOpen = false;
+// }
+// }
